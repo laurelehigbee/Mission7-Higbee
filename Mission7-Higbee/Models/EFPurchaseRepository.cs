8b955982b832903e775acc083f30e5d5023aaac4
laurelehigbee <90651214+laurelehigbee@users.noreply.github.com>
2022-03-09 16:13:05 -0700

    Part 2/Bootstrap Changes

--- Mission7-Higbee/Models/EFPurchaseRepository.cs
+++ Mission7-Higbee/Models/EFPurchaseRepository.cs
@@ -9,13 +9,15 @@ namespace Mission7_Higbee.Models
 {
     public class EFPurchaseRepository : IPurchaseRepository
     {
-        private BookstoreContext context;
+        private BookstoreContext context { get; set; }
 
         public EFPurchaseRepository (BookstoreContext temp)
         {
             context = temp;
         }
         public IQueryable<Purchase> Purchases => context.Purchases.Include(x => x.Lines).ThenInclude(x=>x.Book);
+
+        public IQueryable<Book> Books => context.Books;
 
         public void SavePurchase(Purchase purchase)
         {
@@ -24,6 +26,23 @@ namespace Mission7_Higbee.Models
             {
                 context.Purchases.Add(purchase);
             }
+            context.SaveChanges();
+        }
+
+        public void SaveBook(Book b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Book b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book b)
+        {
+            context.Remove(b);
             context.SaveChanges();
         }
     }
--- Mission7-Higbee/Models/IPurchaseRepository.cs
+++ Mission7-Higbee/Models/IPurchaseRepository.cs
@@ -9,6 +9,10 @@ namespace Mission7_Higbee.Models
     public interface IPurchaseRepository
     {
         IQueryable<Purchase>Purchases { get; }
+        IQueryable<Book> Books { get; }
         void SavePurchase(Purchase purchase);
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
     }
 }
